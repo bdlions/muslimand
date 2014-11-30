946fa3e87193849571e40ed236fe1bca69c78bc0
ahmed10veer <ahmed10veer@gmail.com>
2014-11-30 16:00:29 +0600

    homepage

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -12,51 +12,148 @@
         <script src="<?php echo base_url(); ?>resources/js/jquery-1.11.1.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="<?php echo base_url(); ?>resources/js/bootstrap.min.js"></script>
-
     </head>
     <body>
-        <div class="container-fluid">
-            <?php if (isset($message) && ($message != NULL)): ?>
+
+        <?php if (isset($message) && ($message != NULL)): ?>
             <div class="alert alert-danger alert-dismissible"><?php echo $message; ?></div>
-            <?php endif; ?>
+        <?php endif; ?>
+        <div class="row form-group">
+            <div class="col-md-12">
+                <div class="container-fluid" style="padding-top: 20px; background-color: #62C362; color: white">
+                    <div class="row">
+                        <?php echo form_open("auth/login"); ?>
+                        <div class="col-md-4" style="text-align: center">
+                            <span style="font-size: 50px">Muslimand</span>
+                            <p>Hello Muslims! Keep updated with Muslimand</p>
+                        </div>
+                        <div class="col-md-2">
+                            facebook tweeter
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="control-label">Email/Username:</label> 
+                                <?php echo form_input($identity + array('class' => 'form-control')); ?>
+                            </div>
+                            <div class="form-group">
+                                <input type="checkbox"> <label class="control-label"> Remember Me</label> 
+                            </div>
+
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="control-label">Password:</label>
+                                <?php echo form_input($password + array('class' => 'form-control')); ?>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-default pull-right col-md-5">Log In</button>
+                                <a href="forgot_password" style="font-weight: bolder; color: white"><?php echo lang('login_forgot_password'); ?></a>
+                            </div>
+                        </div>
+                        <?php echo form_close(); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid" style="color: #62C362">
+            <div class="col-md-offset-9 col-md-3 form-group">
+                <span style="font-size: 40px">Sign Up</span>
+            </div>
+            <div class="row form-group">
+                <div class="col-md-offset-1 col-md-6">
+                    <img class="img-responsive" style="width: 100%" src="<?php echo base_url() ?>resources/images/banner.png">
+                </div>
+                <div class="col-md-4" style="padding-left: 0px;">
+                    <?php echo form_open("auth/login", array('id' => '', 'class' => 'form-horizontal')); ?>
+                    <div class="row form-group">
+                        <label class="col-md-5 control-label">First name </label> 
+                        <div class="col-md-7">
+                            <input type="text" class="form-control" id="first_name" placeholder="Enter First_name">
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <label class="col-md-5 control-label">Last name</label> 
+                        <div class="col-md-7">
+                            <input type="text" class="form-control" id="last_name" placeholder="Enter Last_name">
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <label class="col-md-5 control-label">Email</label> 
+                        <div class="col-md-7">
+                            <input type="email" class="form-control" id="email" placeholder="Enter email_address">
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <label class="col-md-5 control-label">Password</label> 
+                        <div class="col-md-7">
+                            <input type="password" class="form-control" id="password" placeholder="Re-enter password">
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <label class="col-md-5 control-label">Confirm Password</label> 
+                        <div class="col-md-7">
+                            <input type="password" class="form-control" id="confirm_password" placeholder="Re-enter password">
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <label class="col-md-5 control-label">Gender</label> 
+                        <div class="col-md-7">
+                            <select class="form-control">
+                                <option class="form-control">
+                                    Male
+                                </option>
+                                <option class="form-control">
+                                    Female
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row form-group">
+                        <div class="col-md-offset-5 col-md-7">
+                            <button type="submit" class="btn btn-success col-md-12">SIGN UP</button> 
+                        </div>
+                    </div>
+                    <?php echo form_close(); ?>
+                </div>
+                <div class="col-md-1">
+
+                </div>
+
+            </div>
+        </div>
+        <div class="container-fluid">
             <div class="row">
-                <?php echo form_open("auth/login"); ?>
-                <div class="col-md-4">
-                    <span style="font-size: x-large"><?php echo lang('login_heading'); ?></span>
-            <p><?php echo lang('login_subheading'); ?></p>
+                <div class="col-md-12">
+                    <div class="col-md-2">kamal</div>
+                    <div class="col-md-2">kamal</div>
+                    <div class="col-md-2">kamal</div>
+                    <div class="col-md-2">kamal</div>
+                    <div class="col-md-2">kamal</div>
+                    <div class="col-md-2">kamal</div>
                 </div>
-                <div class="col-md-2">
-                    asd
-                </div>
-                <div class="col-md-3">
-                    <?php echo lang('login_identity_label', 'identity'); ?>
-                    <?php echo form_input($identity); ?>
-                </div>
-                <div class="col-md-3">
-                    <?php echo lang('login_password_label', 'password'); ?>
-                    <?php echo form_input($password); ?>
-                    <?php echo lang('login_remember_label', 'remember'); ?>
-                    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"'); ?>
-                    <?php echo form_submit('submit', lang('login_submit_btn')); ?>
-                    <a href="forgot_password"><?php echo lang('login_forgot_password'); ?></a>
-                </div>
-                <?php echo form_close(); ?>
             </div>
             <div class="row">
-                <div class="col-md-8">
-                    asda
+                <div class="col-md-12" style="padding: 0px;">
+                    <div style="border: 6px solid lightgray; padding: 10px 0px 10px 0px">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <img class="img-responsive" style="width: 100%" src="<?php echo base_url() ?>resources/images/banner.png">
+                            </div>
+                            <div class="col-md-5">
+                                <div class="row">
+                                    <div class="col-md-12" style="padding: 5px;">
+                                        NAME
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12" style="padding: 5px;">
+                                        <img class="img-responsive" style="width: 100%" src="<?php echo base_url() ?>resources/images/banner.png">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-4">
-                    asdasd
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-2">asd</div>
-                <div class="col-md-2">asd</div>
-                <div class="col-md-2">zxcz</div>
-                <div class="col-md-2"> zxc</div>
-                <div class="col-md-2">zzXczxc </div>
-                <div class="col-md-2">asdasd</div>
             </div>
         </div>
     </body>
