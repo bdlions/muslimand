c79261d8d230e98487897eff966f6747ed88eb8b
ahmed10veer <ahmed10veer@gmail.com>
2014-12-02 20:04:38 +0600

    forgot and wromg pass

--- application/views/auth/forgot_password.php
+++ application/views/auth/forgot_password.php
@@ -1,15 +1,73 @@
-<h1><?php echo lang('forgot_password_heading');?></h1>
-<p><?php echo sprintf(lang('forgot_password_subheading'), $identity_label);?></p>
+<!--
+<h1><?php echo lang('forgot_password_heading'); ?></h1>
+<p><?php echo sprintf(lang('forgot_password_subheading'), $identity_label); ?></p>
 
-<div id="infoMessage"><?php echo $message;?></div>
+<div id="infoMessage"><?php echo $message; ?></div>
 
-<?php echo form_open("auth/forgot_password");?>
+<?php echo form_open("auth/forgot_password"); ?>
 
-      <p>
-      	<label for="email"><?php echo sprintf(lang('forgot_password_email_label'), $identity_label);?></label> <br />
-      	<?php echo form_input($email);?>
-      </p>
+<p>
+    <label for="email"><?php echo sprintf(lang('forgot_password_email_label'), $identity_label); ?></label> <br />
+<?php echo form_input($email); ?>
+</p>
 
-      <p><?php echo form_submit('submit', lang('forgot_password_submit_btn'));?></p>
+<p><?php echo form_submit('submit', lang('forgot_password_submit_btn')); ?></p>
 
-<?php echo form_close();?>
+<?php echo form_close(); ?>
+
+
+-->
+
+
+<div class="row">
+    <div class="col-md-offset-2 col-md-8">
+        <div class="row form-group">
+            <span style="font-size: 30px">Forgot Password?</span>
+        </div>  
+        <?php if (isset($message) && ($message != NULL)): ?>
+            <div class="alert alert-danger alert-dismissible"><?php echo $message; ?></div>
+        <?php endif; ?>
+        <?php echo form_open("auth/forgot_password", array('class' => 'form-horizontal')); ?>
+        <div class="row form-group">  
+            <div class="col-md-12">
+                Select a security question from below:
+            </div>
+        </div>
+        <div class="row form-group">
+            <div class="col-md-6">
+                <select class="form-control">
+                    <option>What is your home town?</option>
+                </select>
+            </div>
+            <div class="col-md-6">
+                <input type="text" class="form-control" id="email" placeholder="Enter answer">  
+            </div>
+        </div>
+        <div class="row form-group">  
+            <div class="col-md-12"></div>
+        </div>
+        <div class="row form-group">  
+            <div class="col-md-12"></div>
+        </div>
+            
+        <div class="row form-group">  
+            <div class="col-md-6">
+                Please enter your Email so we can send you an email to reset your password.
+            </div>
+        </div>
+        <div class="row form-group">
+            <div class="col-md-2">
+                <label class="control-label">Email</label>
+            </div>
+            <div class="col-md-4">
+                <input type="email" class="form-control" id="email" placeholder="Enter email address">  
+            </div>
+        </div>
+        <div class="row form-group">
+            <div class=" col-md-offset-3 col-md-3">
+                <button class="btn btn-danger form-control" type="submit" name="submit_button">Reset Password</button>
+            </div>
+        </div>
+        <?php echo form_close(); ?>
+    </div>
+</div>      
--- application/views/auth/wrong_password.php
+++ application/views/auth/wrong_password.php
@@ -1,8 +1,71 @@
-<?php
+<!--
+<div class="row form-group">  
+    <div class="col-md-4">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    </div>
+</div>
+<div class="row form-group">
+    <label class="col-md-2" class="form-control">Email</label>
+    <div class="col-md-4">
+        <input type="email" class="form-control" id="email" placeholder="Enter Email Address">  
+    </div>
+</div>
+<div class="row form-group">
+    <label class="col-md-2" class="form-control">Password</label>
+    <div class="col-md-4">
+        <input type="password" class="form-control" id="password" placeholder="Enter your password">  
+    </div>
+</div>
 
+<div class="row form-group">
+    <div class=" col-md-offset-5 col-md-2">
+        <button class="btn btn-default">login</button>
+    </div>
+</div>
+<div>
+    <a ref=''>Forget Password?</a>
+</div>
+-->
+
+
+<div class="row">
+    <div class="col-md-offset-2 col-md-8">
+        <div class="row form-group">
+            <span style="font-size: 30px">Wrong password</span>
+        </div>  
+        <?php if (isset($message) && ($message != NULL)): ?>
+            <div class="alert alert-danger alert-dismissible"><?php echo $message; ?></div>
+        <?php endif; ?>
+        <div class="row form-group">  
+            <div class="col-md-12">
+                Please Re-enter your Password
+            </div>
+        </div>
+        <?php echo form_open("auth/forgot_password"); ?>
+
+        <div class="row form-group">
+            <label class="col-md-2" class="control-label">Email</label>
+            <div class="col-md-6">
+                <input type="email" class="form-control" id="email" placeholder="Enter Email Address">  
+            </div>
+        </div>
+        <div class="row form-group">
+            <label class="col-md-2" class="control-label">Password</label>
+            <div class="col-md-6">
+                <input type="password" class="form-control" id="password" placeholder="Enter your password">  
+            </div>
+        </div>
+        <div class="row form-group">
+            <div class=" col-md-offset-4 col-md-4">
+                <button class="btn btn-success form-control">Log In</button>
+            </div>
+        </div>
+        <div class="row form-group">
+            <div class=" col-md-offset-4 col-md-4">
+                <a href="">Forget Password?</a>
+                <!--<button class="btn btn-success form-control">Log In</button>-->
+            </div>
+        </div>
+        <?php echo form_close(); ?>
+    </div>
+</div>      
