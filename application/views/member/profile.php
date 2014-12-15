4d3e0cecb9ee4a607bb009673269fa03411f1987
Redoy <nazmulislamredoy@gmail.com>
2014-12-15 14:44:51 +0600

    profile page

--- application/views/member/profile.php
+++ application/views/member/profile.php
@@ -1,1 +1,111 @@
 
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <link rel="stylesheet" href="css/bootstrap.css"/>
+        <link rel="stylesheet" href="css/my_css.css"/>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+        <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
+        <link rel="stylesheet" href="css/jquery-ui.css">
+        <script src="js/jquery-ui.js"></script>
+
+        <style>
+            .main_wrapper {
+                width: 100%;
+                margin: 0 auto;
+               
+            }
+            .top{ background-color : #703684 ;}
+            .middle{ background-color:#EDF0F5 ;}
+            .footer{background-color: #FFFFFF;}
+        </style>
+
+        <title>Profile Name</title>
+    </head>
+    <body>
+        <div class="main_wrapper id="top">
+            <div class="container-fluid">
+                <div class="row top" style="padding: 20px 0 20px 0;">
+                    <div class="col-md-1">
+                        
+                    </div>
+                 
+                    <div class="col-md-5">
+                        <input type ="text" class="form-control" style=" font-size: 120%"placeholder="Search for people, places and things">
+                    </div>
+                </div>
+                <div class="row middle">
+                    <div class=" col-md-1" style="background-color: #E9EAED; border: 1px solid #DCDCDC;">
+                        </br>
+                        </br>
+                    </div>
+                    <div class=" col-md-7" style=" position: relative; border: 1px solid #DCDCDC;">
+                        <div class=" coverBorder">
+                            <button type="button" class=" btn btn-default" style=" position: absolute; top: 10px; left: 20px; font-size: 80%">Upload photo</button>
+                            <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 20px; right:  140px; font-size: 80%">Update Info</button>
+                            <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 20px; right:  20px; font-size: 80%">View Activity Log</button>
+                            <img src="images/car.jpg" alt="Smiley face" width="100%" height="40%">
+                        </div>
+                        <div class=" btn-group" role="group" aria-label="...">
+                            <button type="button" class=" btn btn-default" style=" font-size: 100%">Timeline</button>
+                            <button type="button" class=" btn btn-default" style=" font-size: 100%">About</button>
+                            <button type="button" class=" btn btn-default" style=" font-size: 100%">Photo</button>
+                            <button type="button" class=" btn btn-default" style=" font-size: 100%">Friends</button>
+                        </div>
+                        <div class="btn-group" role="group">
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="font-size: 100%">
+                                More
+                                <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Places</a></li>
+                                <li><a href="#">Sports</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class=" col-md-1">
+                        <a style=" color: black; font-size: 200%;" href=""> Sponsored</a>
+                        <hr>
+                        <img src="images/Food.jpg"  width="100%" height="20%">
+                        <p></p>
+                        <ul style=" list-style-type: none">
+                            <li><a style=" color: black" href="#">Recent</li></a>
+                            <li><a style=" color: black" href="#">2014</li></a>
+                            <li><a style=" color: black" href="#">2013</li></a>
+                            <li><a style=" color: black" href="#">2012</li></a>
+                            <li><a style=" color: black" href="#">2011</li></a>
+                            <li><a style=" color: black" href="#">2010</li></a>
+                            <li><a style=" color: black" href="#">Born</li></a>
+                        </ul>
+<!--                        <a style=" color: black; font-size: 120%;" href=""> Recent</a></br>
+                        <a style=" color: black; font-size: 120%;" href=""> 2014</a></br>
+                        <a style=" color: black; font-size: 120%;" href=""> 2013</a></br>
+                        <a style=" color: black; font-size: 120%;" href=""> 2012</a></br>
+                        <a style=" color: black; font-size: 120%;" href=""> 2011</a></br>
+                        <a style=" color: black; font-size: 120%;" href=""> 2010</a>-->
+                    </div>
+                    <div class="col-md-3">
+                        <a style=" color: black" href="http://localhost/BootstrapTraining/News_feed.php">Profile Name One hacked your profile</a></br>
+                        <hr>
+                        <a style=" color: black" href="http://localhost/BootstrapTraining/News_feed.php"> Profile Name Three likes your photos</a></br>
+                        <hr>
+                        <img src="images/car.jpg"  width="42" height="42">
+                        <a style=" color: black" href="http://localhost/BootstrapTraining/News_feed.php"> Profile Name One</a></br>
+                        <img src="images/car_1.jpg"  width="42" height="42">
+                        <a style=" color: black" href="http://localhost/BootstrapTraining/News_feed.php"> Profile Name Two</a></br>
+                        <img src="images/Food.jpg"  width="42" height="42">
+                        <a style=" color: black" href="http://localhost/BootstrapTraining/News_feed.php"> Profile Name Three</a></br>
+                    </div>
+                </div>
+                
+                <div class="row footer" style="padding: 20px 0 20px 180px; margin: 45px 0px 0px 0px; ">
+                    <div class="col-md-12">
+                      &COPY; All right reserved
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
