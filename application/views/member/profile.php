854a94d80188e5f8597e50b15e0cd481a5248797
Redoy <nazmulislamredoy@gmail.com>
2014-12-17 13:17:46 +0600

    User profile page

--- application/views/member/profile.php
+++ application/views/member/profile.php
@@ -4,11 +4,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <link rel="stylesheet" href="css/bootstrap.css"/>
         <link rel="stylesheet" href="css/my_css.css"/>
-        <script type="text/javascript" src="js/bootstrap.min.js"></script>
-
         <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
-        <link rel="stylesheet" href="css/jquery-ui.css">
-        <script src="js/jquery-ui.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        
+        
+
 
         <style>
             .main_wrapper {
@@ -28,50 +28,49 @@
             <div class="container-fluid">
                 <div class="row top" style="padding: 20px 0 20px 0;">
                     <div class="col-md-1">
-                        
                     </div>
-                 
                     <div class="col-md-5">
                         <input type ="text" class="form-control" style=" font-size: 120%"placeholder="Search for people, places and things">
                     </div>
                 </div>
                 <div class="row middle">
-                    <div class=" col-md-1" style="background-color: #E9EAED; border: 1px solid #DCDCDC;">
+                    <div class=" col-md-1">
                         </br>
                         </br>
                     </div>
-                    <div class=" col-md-7" style=" position: relative; border: 1px solid #DCDCDC;">
-                        <div class=" coverBorder">
+                    <div class=" col-md-7" style=" background-color: white; position: relative; border: 1px solid #DCDCDC;">
+                        <div class=" coverBorder" >
                             <a id="u_jsonp_12_6" class="profilePicThumb" rel="theater" href="#">
                                 <img style="position: absolute; bottom: 10px; left: 20px; "class="profilePic img" src="images/Food.jpg" width="160" height="160" alt="Profile Name">
                                 <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 40px; left: 24px; font-size: 80%">Upload profile picture</button>
                             </a>
-                            <a  style="position: absolute; bottom: 32px; left: 180px;"class=" btn" href=""> Profile Name</a>
-                            <button type="button" class=" btn btn-default" style=" position: absolute; top: 10px; left: 20px; font-size: 80%">Upload photo</button>
-                            <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 20px; right:  140px; font-size: 80%">Update Info</button>
-                            <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 20px; right:  20px; font-size: 80%">View Activity Log</button>
-                            <img src="images/car.jpg" style="" "alt="Smiley face" width="100%" height="40%">
+                            <a  style="position: absolute; bottom: 32px; left: 180px;"class=" btn" href=""><b>Profile Name</b></a>
+                            <button type="button" class=" btn btn-default" style=" position: absolute; top: 10px; left: 20px; font-size: 80%">Upload cover photo</button>
+                            <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 45px; right:  140px; font-size: 80%">Update Info</button>
+                            <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 45px; right:  20px; font-size: 80%">View Activity Log</button>
+                            <img src="images/car.jpg" style=" margin-left: -15px;"width="104%" height="40%">
                         </div>
-                        <div class=" btn-group" role="group" aria-label="...">
-                            <button type="button" class=" btn btn-default" style=" font-size: 100%">Timeline</button>
-                            <button type="button" class=" btn btn-default" style=" font-size: 100%">About</button>
-                            <button type="button" class=" btn btn-default" style=" font-size: 100%">Photo</button>
-                            <button type="button" class=" btn btn-default" style=" font-size: 100%">Friends</button>
-                        </div>
-                        <div class="btn-group" role="group">
-                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="font-size: 100%">
-                                More
-                                <span class="caret"></span>
-                            </button>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Places</a></li>
-                                <li><a href="#">Sports</a></li>
-                            </ul>
+                        <div class=" btn-group" role="group" aria-label="..." style=" position: relative; left: 175px;">
+                                <button type="button" class=" btn btn-default" style=" font-size: 100%">Timeline</button>
+                                <button type="button" class=" btn btn-default" style=" font-size: 100%">About</button>
+                                <button type="button" class=" btn btn-default" style=" font-size: 100%">Photo</button>
+                                <button type="button" class=" btn btn-default" style=" font-size: 100%">Friends</button>
+                            <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                                    More
+                                    <span class="caret"></span>
+                                </button>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">Groups</a></li>
+                                        <li><a href="#">Sports</a></li>
+                                    </ul>
+                            </div>
                         </div>
                     </div>
+                    <!-- Advertisement Section -->
                     <div class=" col-md-2">
                         <a style=" color: black; font-size: 200%;" href=""> Sponsored</a>
-                        <hr>
+                        <hr style=" border: 1px solid #DCDCDC">
                         <img src="images/Food.jpg"  width="100%" height="20%">
                         <p></p>
                         <ul style=" list-style-type: none">
@@ -83,16 +82,17 @@
                             <li><a style=" color: black" href="#">2010</li></a>
                             <li><a style=" color: black" href="#">Born</li></a>
                         </ul>
-                      
                     </div>
                     <div class="col-md-2" style=" position: relative; border: 1px solid #DCDCDC;">
-                        <div class="col-md-12" style=" position: relative; border: 1px solid #DCDCDC; padding-bottom: 2px;">
+                        
+                        <!-- Recent activities-->
+                        <div class="col-md-12" style=" width: 110%; padding-bottom: 2px;">
                            <button>
                                <div class=" col-md-3">
                                    <img src="images/car.jpg"  width="30" height="30">
                                </div>
                                <div class=" col-md-9">
-                                   <a style=" color: black; font-size: 80%;" href="http://localhost/BootstrapTraining/News_feed.php">Profile Name One hacked your profile</a>
+                                   <a style=" color: black; font-size: 80%;" href="#">Profile Name One hacked your profile</a>
                                </div>
                             </button>
                             <button>
@@ -100,12 +100,13 @@
                                     <img src="images/Food.jpg"  width="30" height="30">
                                 </div>
                                 <div class=" col-md-9">
-                                <a style=" color: black; font-size: 80%" href="http://localhost/BootstrapTraining/News_feed.php"> Profile Name Three likes your photos</a> 
+                                <a style=" color: black; font-size: 80%" href="#"> Profile Name Three likes your photos</a> 
                                 </div>
                             </button> 
                         </div>
                         
-                        <div class="col-md-12" style=" position: relative; border: 1px solid #DCDCDC; padding-top: 2px;">
+                        <!-- Chat Box-->
+                        <div class="col-md-12" style=" padding-top: 2px;">
                             <button>
                                <img src="images/car.jpg"  width="30" height="30">
                                 <a style=" color: black" href="http://localhost/BootstrapTraining/News_feed.php"> Profile Name One</a></br>
