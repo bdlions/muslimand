32c5b5b263b6204b97b67427937162341c06aa1b
Redoy <nazmulislamredoy@gmail.com>
2014-12-17 18:33:34 +0600

    Updated profile page

--- application/views/member/profile.php
+++ application/views/member/profile.php
@@ -6,26 +6,38 @@
         <link rel="stylesheet" href="css/my_css.css"/>
         <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
-        
-        
-
-
         <style>
             .main_wrapper {
                 width: 100%;
                 margin: 0 auto;
-               
+
             }
             .top{ background-color : #703684 ;}
             .middle{ background-color:#EDF0F5 ;}
             .footer{background-color: #FFFFFF;}
+            a:link{
+                color: yellow;
+                text-decoration: none;
+            }
+            a:visited{
+                color: violet;
+                text-decoration: none;
+            }
+            a:hover{
+                color: green;
+                text-decoration: underline;
+            }
+            a:active{
+                color: pink;
+                text-decoration: underline;
+            }
         </style>
-
         <title>Profile Name</title>
     </head>
     <body>
-        <div class="main_wrapper id="top">
+        <div class="main_wrapper top">
             <div class="container-fluid">
+                <!-- Header -->
                 <div class="row top" style="padding: 20px 0 20px 0;">
                     <div class="col-md-1">
                     </div>
@@ -33,102 +45,176 @@
                         <input type ="text" class="form-control" style=" font-size: 120%"placeholder="Search for people, places and things">
                     </div>
                 </div>
+
+                <!-- Middle -->
                 <div class="row middle">
                     <div class=" col-md-1">
-                        </br>
-                        </br>
                     </div>
+
+                    <!-- Cover -->
                     <div class=" col-md-7" style=" background-color: white; position: relative; border: 1px solid #DCDCDC;">
-                        <div class=" coverBorder" >
-                            <a id="u_jsonp_12_6" class="profilePicThumb" rel="theater" href="#">
-                                <img style="position: absolute; bottom: 10px; left: 20px; "class="profilePic img" src="images/Food.jpg" width="160" height="160" alt="Profile Name">
-                                <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 40px; left: 24px; font-size: 80%">Upload profile picture</button>
-                            </a>
-                            <a  style="position: absolute; bottom: 32px; left: 180px;"class=" btn" href=""><b>Profile Name</b></a>
-                            <button type="button" class=" btn btn-default" style=" position: absolute; top: 10px; left: 20px; font-size: 80%">Upload cover photo</button>
-                            <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 45px; right:  140px; font-size: 80%">Update Info</button>
-                            <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 45px; right:  20px; font-size: 80%">View Activity Log</button>
-                            <img src="images/car.jpg" style=" margin-left: -15px;"width="104%" height="40%">
+
+                        <!-- Cover Image -->
+                        <div class=" row">
+
+                            <div class=" col-md-12">
+                                <a id="u_jsonp_12_6" class="profilePicThumb" rel="theater" href="#">
+                                    <img style="position: absolute; bottom: -20px; left: 15px; "class="profilePic img" src="images/Food.jpg" width="160px" height="160px">
+                                    <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 2px; left: 30px; font-size: 80%">Upload profile picture</button>
+                                </a>
+                                <a  style="position: absolute; bottom: 2px; left: 180px;"class=" btn" href=""><b>Profile Name</b></a>
+                                <button type="button" class=" btn btn-default" style=" position: absolute; top: 10px; left: 20px; font-size: 80%">Upload cover photo</button>
+                                <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 20px; right:  140px; font-size: 80%">Update Info</button>
+                                <button type="button" class=" btn btn-default" style=" position: absolute; bottom: 20px; right:  20px; font-size: 80%">View Activity Log</button>
+                                <img src="images/car.jpg" style=" margin-left: -15px;"width="104%" height="52%">
+                            </div>
+
+
+                            <!-- Dropdown Menu --> 
+                            <div class=" row">
+                                <div class=" col-md-12">
+                                    <div class=" coverBorder" >
+                                        <div class=" btn-group" role="group" aria-label="..." style=" position: relative; left: 175px;">
+                                            <button type="button" class=" btn btn-default" style=" font-size: 100%">Timeline</button>
+                                            <button type="button" class=" btn btn-default" style=" font-size: 100%">About</button>
+                                            <button type="button" class=" btn btn-default" style=" font-size: 100%">Photo</button>
+                                            <button type="button" class=" btn btn-default" style=" font-size: 100%">Friends</button>
+                                            <div class="btn-group" role="group">
+                                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                                                    More
+                                                    <span class="caret"></span>
+                                                </button>
+                                                <ul class="dropdown-menu" role="menu">
+                                                    <li><a href="#">Groups</a></li>
+                                                    <li><a href="#">Likes</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div> 
+                                </div>
+                            </div>
                         </div>
-                        <div class=" btn-group" role="group" aria-label="..." style=" position: relative; left: 175px;">
-                                <button type="button" class=" btn btn-default" style=" font-size: 100%">Timeline</button>
-                                <button type="button" class=" btn btn-default" style=" font-size: 100%">About</button>
-                                <button type="button" class=" btn btn-default" style=" font-size: 100%">Photo</button>
-                                <button type="button" class=" btn btn-default" style=" font-size: 100%">Friends</button>
-                            <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                    More
-                                    <span class="caret"></span>
-                                </button>
-                                    <ul class="dropdown-menu" role="menu">
-                                        <li><a href="#">Groups</a></li>
-                                        <li><a href="#">Sports</a></li>
-                                    </ul>
+
+                        <!-- status update-->
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class=" form-group"></div>
+                                <div class="row">
+                                    <div class=" col-md-offset-4 col-md-8">
+                                        <div class=" form-group"></div>
+                                        <p>
+                                            In the end, it's not going to matter how many breaths you took, but how many moments took your breath away
+                                            - shing xiong    
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+
+
                     <!-- Advertisement Section -->
                     <div class=" col-md-2">
-                        <a style=" color: black; font-size: 200%;" href=""> Sponsored</a>
-                        <hr style=" border: 1px solid #DCDCDC">
-                        <img src="images/Food.jpg"  width="100%" height="20%">
-                        <p></p>
-                        <ul style=" list-style-type: none">
-                            <li><a style=" color: black" href="#">Recent</li></a>
-                            <li><a style=" color: black" href="#">2014</li></a>
-                            <li><a style=" color: black" href="#">2013</li></a>
-                            <li><a style=" color: black" href="#">2012</li></a>
-                            <li><a style=" color: black" href="#">2011</li></a>
-                            <li><a style=" color: black" href="#">2010</li></a>
-                            <li><a style=" color: black" href="#">Born</li></a>
-                        </ul>
-                    </div>
-                    <div class="col-md-2" style=" position: relative; border: 1px solid #DCDCDC;">
-                        
-                        <!-- Recent activities-->
-                        <div class="col-md-12" style=" width: 110%; padding-bottom: 2px;">
-                           <button>
-                               <div class=" col-md-3">
-                                   <img src="images/car.jpg"  width="30" height="30">
-                               </div>
-                               <div class=" col-md-9">
-                                   <a style=" color: black; font-size: 80%;" href="#">Profile Name One hacked your profile</a>
-                               </div>
-                            </button>
-                            <button>
-                                <div class=" col-md-3">
-                                    <img src="images/Food.jpg"  width="30" height="30">
-                                </div>
-                                <div class=" col-md-9">
-                                <a style=" color: black; font-size: 80%" href="#"> Profile Name Three likes your photos</a> 
-                                </div>
-                            </button> 
+                        <div class=" row form-group">
+                            <div class=" col-md-12">
+                                <ul style=" list-style-type: none">
+                                    <li><a style=" color: black" href="#">Recent</li></a>
+                                    <li><a style=" color: black" href="#">2014</li></a>
+                                    <li><a style=" color: black" href="#">2013</li></a>
+                                    <li><a style=" color: black" href="#">2012</li></a>
+                                    <li><a style=" color: black" href="#">2011</li></a>
+                                    <li><a style=" color: black" href="#">2010</li></a>
+                                    <li><a style=" color: black" href="#">Born</li></a>
+                                </ul>
+                            </div>
                         </div>
-                        
+                    </div>
+
+                    <div class="col-md-2" style=" position: relative; width: 200px;">
+                        <div class=" row">
+                            <div class=" col-md-12">
+                                <div class=" row">
+                                    <div class=" form-group"></div>
+                                    <div class=" col-md-12">
+                                        <div class=" col-md-3">
+                                            <img src="images/car.jpg"  width="30" height="30"> 
+                                        </div>
+                                        <div class=" col-md-9">
+                                            <a style=" color: black; font-size: 11px;" href="#"><b>Profile Name One</b> hacked your profile</a> 
+                                        </div>
+                                    </div>  
+                                    <div class=" form-group"></div>
+                                </div>
+                                <div class=" row">
+                                    <div class=" form-group"></div>
+                                    <div class=" col-md-12">
+                                        <div class=" col-md-3">
+                                            <img src="images/car_1.jpg"  width="30" height="30"> 
+                                        </div>
+                                        <div class=" col-md-9">
+                                            <a style=" color: black; font-size: 11px;" href="#"><b>Profile Name Three</b> likes your photos</a> 
+                                        </div>
+                                    </div>  
+                                    <div class=" form-group"></div>
+                                </div>  
+                            </div>
+                            <div class=" form-group"></div>
+                        </div>
+
+
                         <!-- Chat Box-->
-                        <div class="col-md-12" style=" padding-top: 2px;">
-                            <button>
-                               <img src="images/car.jpg"  width="30" height="30">
-                                <a style=" color: black" href="http://localhost/BootstrapTraining/News_feed.php"> Profile Name One</a></br>
-                            </button>
-                            <button>
-                                <img src="images/car_1.jpg"  width="30" height="30">
-                                <a style=" color: black" href="http://localhost/BootstrapTraining/News_feed.php"> Profile Name Two</a></br>
-                            </button>
-                            <button>
-                                <img src="images/Food.jpg"  width="30" height="30">
-                                <a style=" color: black" href="http://localhost/BootstrapTraining/News_feed.php"> Profile Name Three</a></br> 
-                            </button>
+                        <div class=" row">
+                            <div class=" form-group"></div>
+                            <div class=" col-md-12">
+                                <div class=" row">
+                                    <div class=" form-group"></div>
+                                    <div class=" col-md-12">
+                                        <div class=" col-md-3">
+                                            <img src="images/car.jpg"  width="30" height="30">
+                                        </div>
+                                        <div class=" col-md-9">
+                                            <a style=" color: black; font-size: 80%" href="#"> Profile Name One</a>
+                                        </div>
+                                    </div>
+                                    <div class=" form-group"></div>
+                                </div>
+                                <div class=" row">
+                                    <div class=" form-group"></div>
+                                    <div class=" col-md-12">
+                                        <div class=" col-md-3">
+                                            <img src="images/Food.jpg"  width="30" height="30">
+                                        </div>
+                                        <div class=" col-md-9">
+                                            <a style=" color: black; font-size: 80%" href="#"> Profile Name One</a>
+                                        </div>
+                                    </div>
+                                    <div class=" form-group"></div>
+                                </div>
+                                <div class=" row">
+                                    <div class=" form-group"></div>
+                                    <div class=" col-md-12">
+                                        <div class=" col-md-3">
+                                            <img src="images/car_1.jpg"  width="30" height="30">
+                                        </div>
+                                        <div class=" col-md-9">
+                                            <a style=" color: black; font-size: 80%" href="#"> Profile Name One</a>
+                                        </div>
+                                    </div>
+                                    <div class=" form-group"></div>
+                                </div>  
+                            </div>
                         </div>
-                    </div>
-                </div>
-                
-                <div class="row footer" style="padding: 20px 0 20px 180px; margin: 45px 0px 0px 0px; ">
-                    <div class="col-md-12">
-                      &COPY; All right reserved
                     </div>
                 </div>
             </div>
+
+            <!-- Footer-->
+            <div class="row footer" style="padding: 20px 0 20px 180px; margin: 45px 0px 0px 0px; ">
+                <div class="col-md-12">
+                    &COPY; All right reserved
+                </div>
+            </div>
         </div>
-    </body>
+    </div>
+</body>
 </html>
