1e078a9fb85a4bbd9d7a395e2d731f1add2068d2
ahmed10veer <ahmed10veer@gmail.com>
2015-02-07 17:10:35 +0600

    friends fixes

--- application/views/member/friends.php
+++ application/views/member/friends.php
@@ -21,81 +21,18 @@
         <div class="pagelet">
             <div class="row form-group">
                 <div class="col-md-12">
-                    <?php for ($i = 0; $i < 4; $i++): ?>
-                        <div style="padding: 10px; display: inline-block;">
-                            <div class="friendd">
-                                <div style="float: left">
-                                    <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_3.jpg"  width="100" height="100">
-                                </div>
-                                <div style="float: left; width: 180px; padding: 10px;">
-                                    <div >
-                                        <a style="font-weight: bold;" href="#">Barak Obama </a>
-                                    </div>
-                                    <div >
-                                        <span style="font-size: 12px">102 Mutual friends </span> 
-                                    </div>
-                                </div>
-                                <div class="pull-right">
-                                    <button style="margin: 10px 10px 0 0;" >Friends</button>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endfor; ?>
-                </div>
-            </div>
-            <div class="row form-group">
-                <div style="padding: 10px; display: inline-block;">
-                    <div class="col-md-6">
-                        <div class="friendd">
-                            <div style="float: left">
-                                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_9.jpg"  width="100" height="100">
-                            </div>
-                            <div style="float: left; width: 180px; padding: 10px;">
-                                <div >
-                                    <a style="font-weight: bold;" href="#">Mohammad Ali</a>
-                                </div>
-                                <div >
-                                    <span style="font-size: 12px">43 Mutual friends </span> 
-                                </div>
-                            </div>
-                            <div class="pull-right">
-                                <button style="margin: 10px 10px 0 0;" >Friends</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="friendd">
-                            <div style="float: left">
-                                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_7.jpg"  width="100" height="100">
-                            </div>
-                            <div style="float: left; width: 180px; padding: 10px;">
-                                <div >
-                                    <a style="font-weight: bold;" href="#">Sharmin Akter</a>
-                                </div>
-                                <div >
-                                    <span style="font-size: 12px">351 friends </span> 
-                                </div>
-                            </div>
-                            <div class="pull-right">
-                                <button style="margin: 10px 10px 0 0;" >Friends</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row form-group">
-                <div style="padding: 10px; display: inline-block;">
-                    <div class="col-md-6">
+                    <?php // for ($i = 0; $i < 12; $i++): ?>
+                    <div style="padding: 10px; display: inline-block;">
                         <div class="friendd">
                             <div style="float: left">
                                 <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_1.jpg"  width="100" height="100">
                             </div>
                             <div style="float: left; width: 180px; padding: 10px;">
                                 <div >
-                                    <a style="font-weight: bold;" href="#">Dr. Belal</a>
+                                    <a style="font-weight: bold;" href="#">Nazrul Islam </a>
                                 </div>
                                 <div >
-                                    <span style="font-size: 12px">33 Mutual friends </span> 
+                                    <span style="font-size: 12px">102 Mutual friends </span> 
                                 </div>
                             </div>
                             <div class="pull-right">
@@ -103,17 +40,17 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
+                    <div style="padding: 10px; display: inline-block;">
                         <div class="friendd">
                             <div style="float: left">
                                 <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_5.jpg"  width="100" height="100">
                             </div>
                             <div style="float: left; width: 180px; padding: 10px;">
                                 <div >
-                                    <a style="font-weight: bold;" href="#">Jannatul Ferdaus </a>
+                                    <a style="font-weight: bold;" href="#">Tanzima Azad </a>
                                 </div>
                                 <div >
-                                    <span style="font-size: 12px">22 Mutual friends </span> 
+                                    <span style="font-size: 12px">102 Mutual friends </span> 
                                 </div>
                             </div>
                             <div class="pull-right">
@@ -121,5 +58,197 @@
                             </div>
                         </div>
                     </div>
+                    <div style="padding: 10px; display: inline-block;">
+                        <div class="friendd">
+                            <div style="float: left">
+                                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_3.jpg"  width="100" height="100">
+                            </div>
+                            <div style="float: left; width: 180px; padding: 10px;">
+                                <div >
+                                    <a style="font-weight: bold;" href="#">Barak Obama </a>
+                                </div>
+                                <div >
+                                    <span style="font-size: 12px">102 Mutual friends </span> 
+                                </div>
+                            </div>
+                            <div class="pull-right">
+                                <button style="margin: 10px 10px 0 0;" >Friends</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="padding: 10px; display: inline-block;">
+                        <div class="friendd">
+                            <div style="float: left">
+                                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_4.jpg"  width="100" height="100">
+                            </div>
+                            <div style="float: left; width: 180px; padding: 10px;">
+                                <div >
+                                    <a style="font-weight: bold;" href="#">Rashida Sultana </a>
+                                </div>
+                                <div >
+                                    <span style="font-size: 12px">102 Mutual friends </span> 
+                                </div>
+                            </div>
+                            <div class="pull-right">
+                                <button style="margin: 10px 10px 0 0;" >Friends</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="padding: 10px; display: inline-block;">
+                        <div class="friendd">
+                            <div style="float: left">
+                                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_1.jpg"  width="100" height="100">
+                            </div>
+                            <div style="float: left; width: 180px; padding: 10px;">
+                                <div >
+                                    <a style="font-weight: bold;" href="#">Nazrul Islam </a>
+                                </div>
+                                <div >
+                                    <span style="font-size: 12px">102 Mutual friends </span> 
+                                </div>
+                            </div>
+                            <div class="pull-right">
+                                <button style="margin: 10px 10px 0 0;" >Friends</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="padding: 10px; display: inline-block;">
+                        <div class="friendd">
+                            <div style="float: left">
+                                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_5.jpg"  width="100" height="100">
+                            </div>
+                            <div style="float: left; width: 180px; padding: 10px;">
+                                <div >
+                                    <a style="font-weight: bold;" href="#">Tanzima Azad </a>
+                                </div>
+                                <div >
+                                    <span style="font-size: 12px">102 Mutual friends </span> 
+                                </div>
+                            </div>
+                            <div class="pull-right">
+                                <button style="margin: 10px 10px 0 0;" >Friends</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="padding: 10px; display: inline-block;">
+                        <div class="friendd">
+                            <div style="float: left">
+                                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_3.jpg"  width="100" height="100">
+                            </div>
+                            <div style="float: left; width: 180px; padding: 10px;">
+                                <div >
+                                    <a style="font-weight: bold;" href="#">Barak Obama </a>
+                                </div>
+                                <div >
+                                    <span style="font-size: 12px">102 Mutual friends </span> 
+                                </div>
+                            </div>
+                            <div class="pull-right">
+                                <button style="margin: 10px 10px 0 0;" >Friends</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="padding: 10px; display: inline-block;">
+                        <div class="friendd">
+                            <div style="float: left">
+                                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_4.jpg"  width="100" height="100">
+                            </div>
+                            <div style="float: left; width: 180px; padding: 10px;">
+                                <div >
+                                    <a style="font-weight: bold;" href="#">Rashida Sultana </a>
+                                </div>
+                                <div >
+                                    <span style="font-size: 12px">102 Mutual friends </span> 
+                                </div>
+                            </div>
+                            <div class="pull-right">
+                                <button style="margin: 10px 10px 0 0;" >Friends</button>
+                            </div>
+                        </div>
+                    </div>
+                    <?php // endfor; ?>
                 </div>
             </div>
+            <!--            <div class="row form-group">
+                            <div style="padding: 10px; display: inline-block;">
+                                <div class="col-md-6">
+                                    <div class="friendd">
+                                        <div style="float: left">
+                                            <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_9.jpg"  width="100" height="100">
+                                        </div>
+                                        <div style="float: left; width: 180px; padding: 10px;">
+                                            <div >
+                                                <a style="font-weight: bold;" href="#">Mohammad Ali</a>
+                                            </div>
+                                            <div >
+                                                <span style="font-size: 12px">43 Mutual friends </span> 
+                                            </div>
+                                        </div>
+                                        <div class="pull-right">
+                                            <button style="margin: 10px 10px 0 0;" >Friends</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="friendd">
+                                        <div style="float: left">
+                                            <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_7.jpg"  width="100" height="100">
+                                        </div>
+                                        <div style="float: left; width: 180px; padding: 10px;">
+                                            <div >
+                                                <a style="font-weight: bold;" href="#">Sharmin Akter</a>
+                                            </div>
+                                            <div >
+                                                <span style="font-size: 12px">351 friends </span> 
+                                            </div>
+                                        </div>
+                                        <div class="pull-right">
+                                            <button style="margin: 10px 10px 0 0;" >Friends</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>-->
+            <!--            <div class="row form-group">
+                            <div style="padding: 10px; display: inline-block;">
+                                <div class="col-md-6">
+                                    <div class="friendd">
+                                        <div style="float: left">
+                                            <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_1.jpg"  width="100" height="100">
+                                        </div>
+                                        <div style="float: left; width: 180px; padding: 10px;">
+                                            <div >
+                                                <a style="font-weight: bold;" href="#">Dr. Belal</a>
+                                            </div>
+                                            <div >
+                                                <span style="font-size: 12px">33 Mutual friends </span> 
+                                            </div>
+                                        </div>
+                                        <div class="pull-right">
+                                            <button style="margin: 10px 10px 0 0;" >Friends</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="friendd">
+                                        <div style="float: left">
+                                            <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_5.jpg"  width="100" height="100">
+                                        </div>
+                                        <div style="float: left; width: 180px; padding: 10px;">
+                                            <div >
+                                                <a style="font-weight: bold;" href="#">Jannatul Ferdaus </a>
+                                            </div>
+                                            <div >
+                                                <span style="font-size: 12px">22 Mutual friends </span> 
+                                            </div>
+                                        </div>
+                                        <div class="pull-right">
+                                            <button style="margin: 10px 10px 0 0;" >Friends</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>-->
+        </div>
+    </div>
+</div>
