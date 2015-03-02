06fdd5a43118b9ca39278395c53dc540c0c419db
ahmed10veer <ahmed10veer@gmail.com>
2015-03-02 15:06:22 +0600

    upload works on these files

--- application/controllers/videos.php
+++ application/controllers/videos.php
@@ -53,8 +53,8 @@ class Videos extends CI_Controller {
     }
     function do_upload() {
         $config['upload_path'] = './uploads/';
-//        $config['allowed_types'] = 'gif|jpg|png';
-//        $config['max_size'] = '100';
+        $config['allowed_types'] = 'gif|jpg|png|mp4';
+        $config['max_size'] = '100000000';
 //        $config['max_width'] = '1024';
 //        $config['max_height'] = '768';
 
