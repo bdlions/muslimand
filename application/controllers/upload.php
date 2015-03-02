bb5fadf03591f789ea91874849dd3be770a91c5e
ahmed10veer <ahmed10veer@gmail.com>
2015-03-02 17:12:42 +0600

    done

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -2,19 +2,16 @@
 
 class Upload extends CI_Controller {
 
-    function __construct()
-    {
-            parent::__construct();
-            $this->load->helper(array('form', 'url'));
+    function __construct() {
+        parent::__construct();
+        $this->load->helper(array('form', 'url'));
     }
 
-    function index()
-    {
-            $this->load->view('upload_form', array('error' => ' ' ));
+    function index() {
+        $this->load->view('upload_form', array('error' => ' '));
     }
 
-    function do_upload()
-    {
+    function do_upload() {
         $config['upload_path'] = './uploads/';
         $config['allowed_types'] = 'gif|jpg|png|mp4';
 //        $config['max_size']	= '100';
@@ -22,33 +19,25 @@ class Upload extends CI_Controller {
 //        $config['max_height']  = '768';
 
         $this->load->library('upload', $config);
-
-        if (!is_dir('uploads'))
-        {
+        if (!is_dir('uploads')) {
             mkdir('./uploads', 0777, true);
         }
         $dir_exist = true; // flag for checking the directory exist or not
-        if (!is_dir('uploads/'))
-        {
+        if (!is_dir('uploads/')) {
             mkdir('./uploads/', 0777, true);
             $dir_exist = false; // dir not exist
+        } else {
         }
-        else{
-
-        }
-
-        if ( ! $this->upload->do_upload())
-        {
-                $error = array('error' => $this->upload->display_errors());
-
-                $this->load->view('upload_form', $error);
-        }
-        else
-        {
-                $data = array('upload_data' => $this->upload->data());
-
-                $this->load->view('upload_success', $data);
+        if (!$this->upload->do_upload()) {
+            $error = array('error' => $this->upload->display_errors());
+            $this->load->view('upload_form', $error);
+        } else {
+            $upload_data = $this->upload->data();
+            exec("ffmpeg -i " . $upload_data['full_path'] . " " . $upload_data['file_path'] . $upload_data['raw_name'] . ".flv");
+            $data = array('upload_data' => $this->upload->data());
+            $this->load->view('upload_success', $data);
         }
     }
 }
+
 ?>
