24c218b88c90a0af9742873b46d6e8e320d010fc
Redoy <nazmulislamredoy@gmail.com>
2015-03-18 11:45:47 +0600

    blog's adding page updated

--- application/views/member/blog/blog_add.php
+++ application/views/member/blog/blog_add.php
@@ -31,14 +31,138 @@
         <div class="row padding_top_over_row">
             <div class="col-md-12">
                 <label>*Post:</label><br>
+                CK Editor
             </div>
         </div>
-        
-        
-        
+        <div class="row padding_top_over_row">
+            <div class="col-md-6">
+                <label>Categories: </label><br>
+                <ul class="li_dot_remove inline">
+                    <li id="all">All</li>
+                    <li id="public">Public</li>
+                    <li id="personal">Personal</li>
+                    <li id="used">Most Used</li>
+                </ul>
+                <div class="pagelet_divider"></div>
+                <ul id="all_li" class="li_dot_remove scrolling_y_axis_style">
+                    <a href=""><li><input id="categoty_1" type="checkbox" value="1"> Book Reviews</li></a>
+                    <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
+                    <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
+                    <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
+                    <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
+                    <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
+                    <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
+                    <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
+                    <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
+                    <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
+                    <a href=""><li><input id="categoty_11" type="checkbox" value="11"> Recreation</li></a>
+                    <a href=""><li><input id="categoty_12" type="checkbox" value="12"> Shopping</li></a>
+                    <a href=""><li><input id="categoty_13" type="checkbox" value="13"> Society</li></a>
+                    <a href=""><li><input id="categoty_14" type="checkbox" value="14"> Sports</li></a>
+                    <a href=""><li><input id="categoty_15" type="checkbox" value="15"> Stories of Phophets</li></a>
+                    <a href=""><li><input id="categoty_16" type="checkbox" value="16"> Technology</li></a>
+                    <a href=""><li><input id="categoty_17" type="checkbox" value="17"> Quran Contest</li></a>
+                </ul>
+                <ul id="public_li" class="li_dot_remove scrolling_y_axis_style">
+                    <a href=""><li><input id="categoty_1" type="checkbox" value="1"> Book Reviews</li></a>
+                    <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
+                    <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
+                    <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
+                    <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
+                    <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
+                    <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
+                    <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
+                    <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
+                    <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
+                    <a href=""><li><input id="categoty_11" type="checkbox" value="11"> Recreation</li></a>
+                    <a href=""><li><input id="categoty_12" type="checkbox" value="12"> Shopping</li></a>
+                    <a href=""><li><input id="categoty_13" type="checkbox" value="13"> Society</li></a>
+                    <a href=""><li><input id="categoty_14" type="checkbox" value="14"> Sports</li></a>
+                    <a href=""><li><input id="categoty_15" type="checkbox" value="15"> Stories of Phophets</li></a>
+                    <a href=""><li><input id="categoty_16" type="checkbox" value="16"> Technology</li></a>
+                    <a href=""><li><input id="categoty_17" type="checkbox" value="17"> Quran Contest</li></a>
+                </ul>
+                <ul id="personal_id" class="li_dot_remove scrolling_y_axis_style">
+                    <a href=""><li><input id="categoty_1" type="checkbox" value="1"> Book Reviews</li></a>
+                    <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
+                    <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
+                    <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
+                    <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
+                    <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
+                    <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
+                    <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
+                    <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
+                    <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
+                </ul>
+                <ul id="used_id" class="li_dot_remove scrolling_y_axis_style">
+                    <a href=""><li><input id="categoty_1" type="checkbox" value="1"> Book Reviews</li></a>
+                    <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
+                    <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
+                    <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
+                    <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
+                    <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
+                    <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
+                    <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
+                    <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
+                    <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
+                    <a href=""><li><input id="categoty_11" type="checkbox" value="11"> Recreation</li></a>
+                    <a href=""><li><input id="categoty_12" type="checkbox" value="12"> Shopping</li></a>
+                </ul>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+        <div class="row padding_top_over_row">
+            <div class="col-md-4">
+                <input type="text" class="form-control" placeholder="Add a New Category">
+                <span style="font-size: 12px;">Separate multiple categories with commas.</span>
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-xs" style="background-color: #703684; color: white; font-weight: bold; padding: 6px 28px;">Add</button>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+        <div class="row padding_top_over_row">
+            <div class="col-md-6">
+                <label> Privacy: </label>
+                <select class="form-control" name="control">
+                    <option selected="1" value="0">Everyone</option>
+                    <option value="1">Friends</option>
+                    <option value="2">Friends of Friends</option>
+                    <option value="3">Only Me</option>
+                    <option value="4">Custom</option>
+                </select>
+                <span style="font-size: 12px;">Control who can see this Blog.</span>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+        <div class="row padding_top_over_row">
+            <div class="col-md-6">
+                <label>Comment Privacy: </label>
+                <select class="form-control" name="control">
+                    <option selected="1" value="0">Everyone</option>
+                    <option value="1">Friends</option>
+                    <option value="2">Friends of Friends</option>
+                    <option value="3">Only Me</option>
+                    <option value="4">Custom</option>
+                </select>
+                <span style="font-size: 12px;">Control who can comment on this Blog.</span>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+        <div class="row padding_top_over_row">
+            <div class="col-md-6">
+                <label>Please, type what you see on the image below to confirm you are not a Robot</label>
+                Capcha<br>
+                <button class="btn btn-xs" style="background-color: #703684; color: white; font-weight: bold; padding: 3px 28px;">Publish</button><br>
+            <span style="font-size: 12px;">* Required Fields </span>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+        <div class="row form-group"></div>
+        <div class="row form-group"></div>
+
         
     </div>
     <div class="col-md-2"></div>
 </div>
 
-    
