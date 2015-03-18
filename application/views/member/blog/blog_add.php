261ed4fd738ac98fd6259d051ed1b1367007c826
ahmed10veer <ahmed10veer@gmail.com>
2015-03-18 14:15:54 +0600

    category menu

--- application/views/member/blog/blog_add.php
+++ application/views/member/blog/blog_add.php
@@ -38,76 +38,16 @@
             <div class="col-md-6">
                 <label>Categories: </label><br>
                 <ul class="li_dot_remove inline">
-                    <li id="all">All</li>
-                    <li id="public">Public</li>
-                    <li id="personal">Personal</li>
-                    <li id="used">Most Used</li>
+                    <li id="cat_all">All</li>
+                    <li id="cat_public">Public</li>
+                    <li id="cat_personal">Personal</li>
+                    <li id="cat_used">Most Used</li>
                 </ul>
+                <div id="category_container">
+                    
+                </div>
                 <div class="pagelet_divider"></div>
-                <ul id="all_li" class="li_dot_remove scrolling_y_axis_style">
-                    <a href=""><li><input id="categoty_1" type="checkbox" value="1"> Book Reviews</li></a>
-                    <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
-                    <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
-                    <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
-                    <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
-                    <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
-                    <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
-                    <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
-                    <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
-                    <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
-                    <a href=""><li><input id="categoty_11" type="checkbox" value="11"> Recreation</li></a>
-                    <a href=""><li><input id="categoty_12" type="checkbox" value="12"> Shopping</li></a>
-                    <a href=""><li><input id="categoty_13" type="checkbox" value="13"> Society</li></a>
-                    <a href=""><li><input id="categoty_14" type="checkbox" value="14"> Sports</li></a>
-                    <a href=""><li><input id="categoty_15" type="checkbox" value="15"> Stories of Phophets</li></a>
-                    <a href=""><li><input id="categoty_16" type="checkbox" value="16"> Technology</li></a>
-                    <a href=""><li><input id="categoty_17" type="checkbox" value="17"> Quran Contest</li></a>
-                </ul>
-                <ul id="public_li" class="li_dot_remove scrolling_y_axis_style">
-                    <a href=""><li><input id="categoty_1" type="checkbox" value="1"> Book Reviews</li></a>
-                    <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
-                    <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
-                    <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
-                    <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
-                    <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
-                    <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
-                    <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
-                    <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
-                    <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
-                    <a href=""><li><input id="categoty_11" type="checkbox" value="11"> Recreation</li></a>
-                    <a href=""><li><input id="categoty_12" type="checkbox" value="12"> Shopping</li></a>
-                    <a href=""><li><input id="categoty_13" type="checkbox" value="13"> Society</li></a>
-                    <a href=""><li><input id="categoty_14" type="checkbox" value="14"> Sports</li></a>
-                    <a href=""><li><input id="categoty_15" type="checkbox" value="15"> Stories of Phophets</li></a>
-                    <a href=""><li><input id="categoty_16" type="checkbox" value="16"> Technology</li></a>
-                    <a href=""><li><input id="categoty_17" type="checkbox" value="17"> Quran Contest</li></a>
-                </ul>
-                <ul id="personal_id" class="li_dot_remove scrolling_y_axis_style">
-                    <a href=""><li><input id="categoty_1" type="checkbox" value="1"> Book Reviews</li></a>
-                    <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
-                    <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
-                    <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
-                    <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
-                    <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
-                    <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
-                    <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
-                    <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
-                    <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
-                </ul>
-                <ul id="used_id" class="li_dot_remove scrolling_y_axis_style">
-                    <a href=""><li><input id="categoty_1" type="checkbox" value="1"> Book Reviews</li></a>
-                    <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
-                    <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
-                    <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
-                    <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
-                    <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
-                    <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
-                    <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
-                    <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
-                    <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
-                    <a href=""><li><input id="categoty_11" type="checkbox" value="11"> Recreation</li></a>
-                    <a href=""><li><input id="categoty_12" type="checkbox" value="12"> Shopping</li></a>
-                </ul>
+                
             </div>
             <div class="col-md-6"></div>
         </div>
@@ -165,4 +105,107 @@
     </div>
     <div class="col-md-2"></div>
 </div>
-
+<div style="display: none">
+    <div id="all_li"><ul class="li_dot_remove scrolling_y_axis_style">
+        <a href=""><li><input id="categoty_1" type="checkbox" value="1"> all_li Book Reviews</li></a>
+        <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
+        <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
+        <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
+        <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
+        <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
+        <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
+        <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
+        <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
+        <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
+        <a href=""><li><input id="categoty_11" type="checkbox" value="11"> Recreation</li></a>
+        <a href=""><li><input id="categoty_12" type="checkbox" value="12"> Shopping</li></a>
+        <a href=""><li><input id="categoty_13" type="checkbox" value="13"> Society</li></a>
+        <a href=""><li><input id="categoty_14" type="checkbox" value="14"> Sports</li></a>
+        <a href=""><li><input id="categoty_15" type="checkbox" value="15"> Stories of Phophets</li></a>
+        <a href=""><li><input id="categoty_16" type="checkbox" value="16"> Technology</li></a>
+        <a href=""><li><input id="categoty_17" type="checkbox" value="17"> Quran Contest</li></a>
+    </ul></div>
+    <div id="public_li"><ul class="li_dot_remove scrolling_y_axis_style">
+        <a href=""><li><input id="categoty_1" type="checkbox" value="1"> public_li Book Reviews</li></a>
+        <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
+        <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
+        <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
+        <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
+        <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
+        <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
+        <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
+        <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
+        <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
+        <a href=""><li><input id="categoty_11" type="checkbox" value="11"> Recreation</li></a>
+        <a href=""><li><input id="categoty_12" type="checkbox" value="12"> Shopping</li></a>
+        <a href=""><li><input id="categoty_13" type="checkbox" value="13"> Society</li></a>
+        <a href=""><li><input id="categoty_14" type="checkbox" value="14"> Sports</li></a>
+        <a href=""><li><input id="categoty_15" type="checkbox" value="15"> Stories of Phophets</li></a>
+        <a href=""><li><input id="categoty_16" type="checkbox" value="16"> Technology</li></a>
+        <a href=""><li><input id="categoty_17" type="checkbox" value="17"> Quran Contest</li></a>
+    </ul></div>
+    <div id="personal_id"><ul class="li_dot_remove scrolling_y_axis_style">
+        <a href=""><li><input id="categoty_1" type="checkbox" value="1"> personal_id Book Reviews</li></a>
+        <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
+        <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
+        <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
+        <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
+        <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
+        <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
+        <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
+        <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
+        <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
+    </ul></div>
+    <div id="used_id"><ul class="li_dot_remove scrolling_y_axis_style">
+        <a href=""><li><input id="categoty_1" type="checkbox" value="1"> used_id Book Reviews</li></a>
+        <a href=""><li><input id="categoty_2" type="checkbox" value="2"> Business</li></a>
+        <a href=""><li><input id="categoty_3" type="checkbox" value="3"> Stories</li></a>
+        <a href=""><li><input id="categoty_4" type="checkbox" value="4"> Dawah</li></a>
+        <a href=""><li><input id="categoty_5" type="checkbox" value="5"> Education</li></a>
+        <a href=""><li><input id="categoty_6" type="checkbox" value="6"> Arts</li></a>
+        <a href=""><li><input id="categoty_7" type="checkbox" value="7"> Entertainment</li></a>
+        <a href=""><li><input id="categoty_8" type="checkbox" value="8"> Family & Home</li></a>
+        <a href=""><li><input id="categoty_9" type="checkbox" value="9"> Health & Fitness</li></a>
+        <a href=""><li><input id="categoty_10" type="checkbox" value="10"> Interview</li></a>
+        <a href=""><li><input id="categoty_11" type="checkbox" value="11"> Recreation</li></a>
+        <a href=""><li><input id="categoty_12" type="checkbox" value="12"> Shopping</li></a>
+    </ul></div>
+</div>
+<style>
+    #category_container{
+        width: 100%; background-color: white; padding: 10px; margin-top: -7px;
+    }
+    
+</style>
+<script>
+    var backcol;
+    $('.inline li:first').css('background-color', 'white');
+    $('#category_container').html( $('#all_li').html() );
+    $('.inline li').hover(
+            function(){
+                backcol = $(this).css('background-color');
+//                alert(backcol);
+                $(this).css('background-color', 'wheat');
+            },
+            function(){
+                $(this).css('background-color', backcol);
+            }
+                    );
+    $('.inline li').click(function(){
+        $('.inline li').css('background-color', 'transparent');
+        $(this).css('background-color', 'white');
+                backcol = $(this).css('background-color');
+    });
+    $('#cat_all').click(function (){
+        $('#category_container').html( $('#all_li').html() );
+    });
+    $('#cat_public').click(function (){
+        $('#category_container').html( $('#public_li').html() );
+    });
+    $('#cat_personal').click(function (){
+        $('#category_container').html( $('#personal_id').html() );
+    });
+    $('#cat_used').click(function (){
+        $('#category_container').html( $('#used_id').html() );
+    });
+</script>
