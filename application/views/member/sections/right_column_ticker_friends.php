3768e70e6d41ff15ced1e2a8941bc0b8439c2d66
Redoy <nazmulislamredoy@gmail.com>
2015-03-30 13:34:22 +0600

    message box updates (completed)

--- application/views/member/sections/right_column_ticker_friends.php
+++ application/views/member/sections/right_column_ticker_friends.php
@@ -75,17 +75,17 @@
 <div style="height: 50%; overflow-x:hidden; overflow-y: scroll">
     <div class="row">
         <div class="col-md-12">
-            <div class="ticker_friends message_friends_divider_others">
+            <div class="ticker_friends message_friends_divider_others" onclick="open_message_window_1()">
                 <div class="row">
                     <div class="col-md-3">
-                        <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_1.jpg"  width="30" height="30">
+                        <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_8.jpg"  width="30" height="30">
                     </div>
                     <div class="col-md-9">
-                        <h6><b>Dr. Belal</b></h6>
+                        <h6><b>Mohammad Rafique</b></h6>
                     </div>
                 </div> 
             </div>
-            <div class="ticker_friends message_friends_divider_others">
+            <div class="ticker_friends message_friends_divider_others" onclick="open_message_window_2()">
                 <div class="row">
                     <div class="col-md-3">
                         <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_7.jpg"  width="30" height="30">
@@ -95,13 +95,13 @@
                     </div>
                 </div>
             </div>
-            <div class="ticker_friends message_friends_divider_others">
+            <div class="ticker_friends message_friends_divider_others" onclick="open_message_window_3()">
                 <div class="row">
                     <div class="col-md-3">
-                        <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_8.jpg"  width="30" height="30">
+                        <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_1.jpg"  width="30" height="30">
                     </div>
                     <div class="col-md-9">
-                        <h6><b>Mohammad Rafique</b></h6>
+                        <h6><b>Dr. Belal</b></h6>
                     </div>
                 </div> 
             </div>
@@ -148,34 +148,264 @@
         </div>
     </div>
 </div>
-<!--<div style="position: absolute; right: 100%; bottom: 0px; width: 400%; height: 1px">
-    <div class="row" style="position: absolute; bottom: 0px;">
-        <div class="col-xs-3">
-            <div class="msg">
-                a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a
+<div style="position: absolute; right: 100%; bottom: 0; width: 360%; font-size: 10px;">
+    <div class="row" style="position: relative; bottom: 0px; margin: 5px;">
+        <div class="col-md-4">
+            <div id="msg_window_1" class="msg" style="display: none;">
+                <div class="message_friends_divider_full">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <img class="img-responsive" style="margin-top: 3px;" src="<?php echo base_url() ?>resources/images/online.png">
+                        </div>
+                        <div class="col-md-6">
+                            <a style="color:#fff;" href="<?php echo base_url(); ?>member/profile">Dr. Belal</a>
+                        </div>
+                        <div class="col-md-2">
+                            <img class="img-responsive" style="margin-top: 3px; cursor: pointer" src="<?php echo base_url() ?>resources/images/settings.png">
+                        </div>
+                        <div class="col-md-2">
+                            <button type="button" class="close" aria-label="Close" onclick="close_message_window_1()"><span aria-hidden="true">&times;</span></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="message_friends_divider_others chat_box_1_container" style="height: 150px; width: 100%; overflow-y: scroll; overflow-x: hidden; background-color: #fff;">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_owner_style pull-right">
+                                <span class="message_owner_content_style">Hello, Mr. Belal. Myself Azhar. How are you? </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_owner_style pull-right">
+                                <span class="message_owner_content_style">Hi. I'm fine. How are you?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_friend_style pull-left">
+                                <span class="message_friend_content_style">Fine. Are you a doctor?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_friend_style pull-left">
+                                <span class="message_friend_content_style">Yes, I am a professor of doctor. And you?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_owner_style pull-right">
+                                <span class="message_owner_content_style">I'm web a designer & software Engineer of Sampan-IT </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="textarea_background_style">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="col-md-12"><textarea class="chatting_textarea_style" rows="1"></textarea></div>
+                        </div>
+                        <div class="col-md-2">
+                            <form action="upload.php" method="post" style="margin-left: 5px; margin-top: 5px;">
+                                <label for="fileToUpload">
+                                    <img style="cursor: pointer;" src="<?php echo base_url() ?>resources/images/camera.png" />
+                                </label>
+                                <input type="File" name="fileToUpload" id="fileToUpload" class="hidden">
+                            </form>
+                        </div>
+                        <div class="col-md-2">
+                            <img style="cursor: pointer; margin-left: -12px; margin-top: 5px;" src="<?php echo base_url() ?>resources/images/emotion.png" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-xs-3">
-            <div class="msg">
-                a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a
+        <div class="col-md-4">
+            <div id="msg_window_2" class="msg" style="display: none;">
+                <div class="message_friends_divider_full">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <img class="img-responsive" style="margin-top: 3px;" src="<?php echo base_url() ?>resources/images/online.png">
+                        </div>
+                        <div class="col-md-6">
+                            <a style="color:#fff;" href="<?php echo base_url(); ?>member/profile">Sharmin Akter</a>
+                        </div>
+                        <div class="col-md-2">
+                            <img class="img-responsive" style="margin-top: 3px; cursor: pointer" src="<?php echo base_url() ?>resources/images/settings.png">
+                        </div>
+                        <div class="col-md-2">
+                            <button type="button" class="close" aria-label="Close" onclick="close_message_window_2()"><span aria-hidden="true">&times;</span></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="message_friends_divider_others chat_box_1_container" style="height: 150px; width: 100%; overflow-y: scroll; overflow-x: hidden; background-color: #fff;">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_owner_style pull-right">
+                                <span class="message_owner_content_style">Hi, Sharmin!!! How are you? Myself Azhar.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_owner_style pull-right">
+                                <span class="message_owner_content_style">Hello. I'm fine. What about you?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_friend_style pull-left">
+                                <span class="message_friend_content_style">Going well. Are you a student?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_friend_style pull-left">
+                                <span class="message_friend_content_style">Yap. BBA 8th Semester, ABC University. You?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_owner_style pull-right">
+                                <span class="message_owner_content_style">I'm web a designer & software Engineer of Sampan-IT </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="textarea_background_style">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="col-md-12"><textarea class="chatting_textarea_style" rows="1"></textarea></div>
+                        </div>
+                        <div class="col-md-2">
+                            <form action="upload.php" method="post" style="margin-left: 5px; margin-top: 5px;">
+                                <label for="fileToUpload">
+                                    <img style="cursor: pointer;" src="<?php echo base_url() ?>resources/images/camera.png" />
+                                </label>
+                                <input type="File" name="fileToUpload" id="fileToUpload" class="hidden">
+                            </form>
+                        </div>
+                        <div class="col-md-2">
+                            <img style="cursor: pointer; margin-left: -12px; margin-top: 5px;" src="<?php echo base_url() ?>resources/images/emotion.png" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-xs-3">
-            <div class="msg">
-                a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a
-            </div>
-        </div>
-        <div class="col-xs-3">
-            <div class="msg">
-                a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a
+        <div class="col-md-4">
+            <div id="msg_window_3" class="msg" style="display: none;">
+                <div class="message_friends_divider_full">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <img class="img-responsive" style="margin-top: 3px;" src="<?php echo base_url() ?>resources/images/online.png">
+                        </div>
+                        <div class="col-md-6">
+                            <a style="color:#fff;" href="<?php echo base_url(); ?>member/profile">Mohammad Rafique</a>
+                        </div>
+                        <div class="col-md-2">
+                            <img class="img-responsive" style="margin-top: 3px; cursor: pointer" src="<?php echo base_url() ?>resources/images/settings.png">
+                        </div>
+                        <div class="col-md-2">
+                            <button type="button" class="close" aria-label="Close" onclick="close_message_window_3()"><span aria-hidden="true">&times;</span></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="message_friends_divider_others chat_box_1_container" style="height: 150px; width: 100%; overflow-y: scroll; overflow-x: hidden; background-color: #fff;">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_owner_style pull-right">
+                                <span class="message_owner_content_style">As salamualaikum, Rafique vai. How are you? Myself Mohammad Azhar Uddin.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_owner_style pull-right">
+                                <span class="message_owner_content_style">Walaikum as salam. I'm fine. What about you?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_friend_style pull-left">
+                                <span class="message_friend_content_style">Alhamdulillah. What are you doing by profession?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_friend_style pull-left">
+                                <span class="message_friend_content_style">I am a professor of X College . What are you doing?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="message_owner_style pull-right">
+                                <span class="message_owner_content_style">I'm web a designer & software Engineer of Sampan-IT </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="textarea_background_style">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="col-md-12"><textarea class="chatting_textarea_style" rows="1"></textarea></div>
+                        </div>
+                        <div class="col-md-2">
+                            <form action="upload.php" method="post" style="margin-left: 5px; margin-top: 5px;">
+                                <label for="fileToUpload">
+                                    <img style="cursor: pointer;" src="<?php echo base_url() ?>resources/images/camera.png" />
+                                </label>
+                                <input type="File" name="fileToUpload" id="fileToUpload" class="hidden">
+                            </form>
+                        </div>
+                        <div class="col-md-2">
+                            <img style="cursor: pointer; margin-left: -12px; margin-top: 5px;" src="<?php echo base_url() ?>resources/images/emotion.png" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 <style>
     .msg{
-        height: 300px;
-        background-color: red;
+        background-color: #703684;
     }
 </style>
--->
+
+<script type="text/javascript">
+    $(function() {
+        $(".chat_box_1_container").scrollTop($(".chat_box_1_container").height());
+
+    });
+    function close_message_window_1() {
+        $("#msg_window_1").hide();
+    }
+    function close_message_window_2() {
+        $("#msg_window_2").hide();
+    }
+    function close_message_window_3() {
+        $("#msg_window_3").hide();
+    }
+    function open_message_window_1() {
+        $("#msg_window_3").show();
+    }
+    function open_message_window_2() {
+        $("#msg_window_2").show();
+    }
+    function open_message_window_3() {
+        $("#msg_window_1").show();
+    }
+
+
+</script>
---
+++ application/views/member/sections/right_column_ticker_friends_old.php
@@ -0,0 +1,150 @@
+<div style="height: 50%; overflow-x:hidden; overflow-y: scroll">
+    <div class="ticker_friends message_friends_divider_others">
+        <div class="row">
+            <div class="col-md-2">
+                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_8.jpg"  width="30" height="30"> 
+            </div>
+            <div class="col-md-10">
+                <b>Mohammad Rafique</b></a> shared a video.
+            </div>
+        </div>
+    </div>
+    <div class="ticker_friends message_friends_divider_others">
+        <div class="row">
+            <div class="col-md-2">
+                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_5.jpg"  width="30" height="30"> 
+            </div>
+            <div class="col-md-10">
+                <b>Jannatul Ferdaus</b></a> changed her profile pic.
+            </div>
+        </div>
+    </div>
+    <div class="ticker_friends message_friends_divider_others">
+        <div class="row">
+            <div class="col-md-2">
+                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_7.jpg"  width="30" height="30"> 
+            </div>
+            <div class="col-md-10">
+                <b>Sharmin Akter</b></a> likes your comment.
+            </div>
+        </div>
+    </div>
+    <div class="ticker_friends message_friends_divider_others">
+        <div class="row">
+            <div class="col-md-2">
+                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_1.jpg"  width="30" height="30"> 
+            </div>
+            <div class="col-md-10">
+                <b>Dr. Belal</b></a> shared a video.
+            </div>
+        </div>
+    </div>
+    <div class="ticker_friends message_friends_divider_others">
+        <div class="row">
+            <div class="col-md-2">
+                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_4.jpg"  width="30" height="30"> 
+            </div>
+            <div class="col-md-10">
+                <b>Maria Islam</b></a> likes your photos. 
+            </div>
+        </div>
+    </div>
+    <div class="ticker_friends message_friends_divider_others">
+        <div class="row">
+            <div class="col-md-2">
+                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_3.jpg"  width="30" height="30"> 
+            </div>
+            <div class="col-md-10">
+                <b>Barak Obama</b></a> likes your photos. 
+            </div>
+        </div> 
+    </div>
+    <div class="ticker_friends message_friends_divider_others">
+        <div class="row">
+            <div class="col-md-2">
+                <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_6.jpg"  width="30" height="30"> 
+            </div>
+            <div class="col-md-10">
+                <b>Fatematul Kobra</b></a> likes your comments. 
+            </div>
+        </div> 
+    </div>
+</div>
+<!--Chat box-->
+<div style="border-bottom: 2px solid lightgray"></div>
+<div style="height: 50%; overflow-x:hidden; overflow-y: scroll">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="ticker_friends message_friends_divider_others">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_1.jpg"  width="30" height="30">
+                    </div>
+                    <div class="col-md-9">
+                        <h6><b>Dr. Belal</b></h6>
+                    </div>
+                </div> 
+            </div>
+            <div class="ticker_friends message_friends_divider_others">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_7.jpg"  width="30" height="30">
+                    </div>
+                    <div class="col-md-9">
+                        <h6><b> Sharmin Akter</b></h6>
+                    </div>
+                </div>
+            </div>
+            <div class="ticker_friends message_friends_divider_others">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_8.jpg"  width="30" height="30">
+                    </div>
+                    <div class="col-md-9">
+                        <h6><b>Mohammad Rafique</b></h6>
+                    </div>
+                </div> 
+            </div>
+            <div class="ticker_friends message_friends_divider_others">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_6.jpg"  width="30" height="30">
+                    </div>
+                    <div class="col-md-9">
+                        <h6><b>Fatematul Kobra</b></h6>
+                    </div>
+                </div>
+            </div>
+            <div class="ticker_friends message_friends_divider_others">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_3.jpg"  width="30" height="30">
+                    </div>
+                    <div class="col-md-9">
+                        <h6><b>Barak Obama</b></h6>
+                    </div>
+                </div>
+            </div>
+            <div class="ticker_friends message_friends_divider_others">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_5.jpg"  width="30" height="30">
+                    </div>
+                    <div class="col-md-9">
+                        <h6><b>Jannatul Ferdaus</b></h6>
+                    </div>
+                </div>
+            </div>
+            <div class="ticker_friends message_friends_divider_others">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="<?php echo base_url(); ?>resources/images/user_data/profile_pictures/profile_pictures_4.jpg"  width="30" height="30">
+                    </div>
+                    <div class="col-md-9">
+                        <h6><b>Maria Islam</b></h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
