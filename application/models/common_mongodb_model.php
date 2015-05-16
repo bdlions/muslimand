9c5cc3d60cf755d69565e1e5c1df68ca1d5c00aa
nazmulhasan <nazmul@apurbatech.com>
2015-05-16 17:27:26 +0600

    common model for mongodb

---
+++ application/models/common_mongodb_model.php
@@ -0,0 +1,16 @@
+<?php
+class Common_mongodb_model extends Ion_auth_mongodb_model
+{
+    public function __construct() 
+    {
+        parent::__construct();
+    } 
+    
+    /*
+     * This method will return country list from the database
+     */
+    public function get_all_countries()
+    {
+        
+    }
+}
