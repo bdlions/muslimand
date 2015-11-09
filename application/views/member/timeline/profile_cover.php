d48b42016e37f3dd1dde9a7028a81854c7ae684b
rashidacse <rashida57pust@gmail.com>
2015-11-09 14:25:51 +0600

    profile cover bug fix

--- application/views/member/timeline/profile_cover.php
+++ application/views/member/timeline/profile_cover.php
@@ -123,7 +123,7 @@
                             <span class="caret"></span>
                         </button>-->
                         <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-                            <li role="presentation"><a role="menuitem" tabindex="-1" onclick="approve_request('<?php echo $friend_id; ?>')">Confirm</a></li>
+                            <li role="presentation"><a role="menuitem" tabindex="-1" id="request_accept_id" onclick="approve_request('<?php echo $friend_id; ?>')">Confirm</a></li>
                             <li role="presentation"><a role="menuitem" tabindex="-1" ng-click="deleteRequest('<?php echo $friend_id; ?>')">Delete Request</a></li>
                         </ul>
                     </div>
