f3bfe2cb41ba42c4d76dec4a9a7ecde14b16a735
nazmulhasan <nazmul@apurbatech.com>
2015-12-08 23:56:11 +0600

    photo and video categories for the admin panel

---
+++ application/controllers/admin/media.php
@@ -0,0 +1,52 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Media extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->helper('url');        
+    }
+    
+    public function photo_categories()
+    {
+        
+    }
+
+    public function create_photo_category()
+    {
+        
+    }
+    
+    public function update_photo_category()
+    {
+        
+    }
+    
+    public function delete_photo_category()
+    {
+        
+    }
+    
+    public function video_categories()
+    {
+        
+    }
+
+    public function create_video_category()
+    {
+        
+    }
+    
+    public function update_video_category()
+    {
+        
+    }
+    
+    public function delete_video_category()
+    {
+        
+    }
+}
