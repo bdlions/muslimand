f45871dabbb424c330620f327e069ef614c164ee
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2016-01-20 15:47:56 +0600

    friend list member & non member bug fixing

--- application/views/member/timeline/friend_list.php
+++ application/views/member/timeline/friend_list.php
@@ -2,20 +2,22 @@
     <div class="row">
         <div class="col-md-12">
             <a class="non_friend_pagelet_header_anchor_style" href="">Friends</a> 
-<!--            ·  <a class="non_friend_pagelet_header_anchor_style" href="">207</a>  
-            <a class="non_friend_pagelet_header_anchor_style" href="">(1 Mutual)</a>-->
+            <!--            ·  <a class="non_friend_pagelet_header_anchor_style" href="">207</a>  
+                        <a class="non_friend_pagelet_header_anchor_style" href="">(1 Mutual)</a>-->
         </div>
     </div>
 </div>
 <div class="pagelet" style="border: 1px solid #fff;">
     <div class="row" >
-        <div class="col-md-4" ng-repeat="friend in friends" style="padding-bottom: 28px;">
+        <div class="col-md-12">
+            <div ng-repeat="friend in friends" style="padding-bottom: 28px; width: 33.33%; float: left; " >
                 <a href='<?php echo base_url(); ?>member/timeline/{{friend.userId}}' style="text-decoration: none;">
                     <img fallback-src="<?php echo base_url() . PROFILE_PICTURE_PATH_W50_H50; ?>50x50_{{friend.genderId}}.jpg" alt="" ng-src="<?php echo base_url() . PROFILE_PICTURE_PATH_W50_H50; ?>{{friend.userId}}.jpg" />
                 </a>
                 <a href='<?php echo base_url(); ?>member/timeline/{{friend.userId}}' style="text-decoration: none;">
                     <div class="img_caption">{{friend.firstName + " " + friend.lastName}}</div>
                 </a>
+            </div>
         </div>
     </div>
 </div>
--- application/views/non_member/friend_list.php
+++ application/views/non_member/friend_list.php
@@ -2,20 +2,22 @@
     <div class="row">
         <div class="col-md-12">
             <a class="non_friend_pagelet_header_anchor_style" href="">Friends</a> ·
-<!--            <a class="non_friend_pagelet_header_anchor_style" href="">207</a>  
-            <a class="non_friend_pagelet_header_anchor_style" href=""></a>-->
+            <!--            <a class="non_friend_pagelet_header_anchor_style" href="">207</a>  
+                        <a class="non_friend_pagelet_header_anchor_style" href=""></a>-->
         </div>
     </div>
 </div>
 <div class="pagelet" style="border: 1px solid #fff;">
     <div class="row">
-        <div class="col-md-4" ng-repeat="friend in friends" style="padding-bottom: 28px;">
-            <div class="friend_list_img_style">
-                <!--<a href='<?php // echo base_url(); ?>member/timeline/{{friend.userId}}' >-->
+        <div class="col-md-12">
+            <div ng-repeat="friend in friends" style="padding-bottom: 28px; width: 33.33%; float: left; ">
+                <div class="friend_list_img_style">
+                    <!--<a href='<?php // echo base_url();  ?>member/timeline/{{friend.userId}}' >-->
 
                     <img fallback-src="<?php echo base_url() . PROFILE_PICTURE_PATH_W50_H50; ?>50x50_{{friend.genderId}}.jpg" alt="" ng-src="<?php echo base_url() . PROFILE_PICTURE_PATH_W50_H50; ?>{{friend.userId}}.jpg" />
-                    <span class="img_caption">{{friend.firstName}}&nbsp;{{friend.lastName}}</span>
-                <!--</a>-->
+                    <div class="img_caption">{{friend.firstName + " " + friend.lastName}}</div>
+                    <!--</a>-->
+                </div>
             </div>
         </div>
     </div>
